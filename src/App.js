9078949b4de7da6960ae16b4ff9da70ee85a3b62
Lee-d0 <marmar11137@gmail.com>
2024-10-03 14:55:32 -0400

    final change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {useEffect, useState } from  "react"
+import "./App.css"
+import MovieCard from "./components/MovieCard"
+// import {v4 as uuidv4 } from "uuid"
 
 function App() {
+  const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=3c4a25a2a0096d9767d941dd61100c1b"
+
+  const [movies, setMovies] = useState([])
+  const [search, setSearch] = useState('')
+  
+ 
+  useEffect(() => {
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(data => setMovies(data.results))
+  },[])
+
+  console.log(movies)
+
+
+ const handleSearch = (e) => {
+      e.preventDefault()
+
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzRhMjVhMmEwMDk2ZDk3NjdkOTQxZGQ2MTEwMGMxYiIsIm5iZiI6MTcyNzk3NjM5Ni43NzE4NzUsInN1YiI6IjY2YjM5Nzg2OTE0NzA1ZTc5MDZmNzYxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vABsxSn5ixpC4n8Q4T8H_LavkFV7ZT_Uq1ntIhQwdSU'
+        }
+      };
+      
+      fetch(`https://api.themoviedb.org/3/search/movie?query=${search}&include_adult=false&language=en-US&page=1`, options)
+        .then(response => response.json())
+        .then(responce => setMovies(responce.results))
+        .catch(err => console.error(err));
+      
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="search_nav">
+        <div className="title">
+          <h1>Movies</h1>
+        </div>
+        <div className="search_box">
+            <form onSubmit={handleSearch}>
+              <input onChange={(e) => setSearch(e.target.value)}/>
+              <button>Search</button>
+            </form>
+          </div>
+        
+      </div>
+      <div className="movies">
+          {movies.map((movie) => 
+            <MovieCard {...movie} />
+          )}
+      </div>
+      
+      
     </div>
   );
 }
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./MovieCard.css"
+
+const MovieCard = (movie) => {
+
+const API_IMG = "https://image.tmdb.org/t/p/w500/"
+
+  return (
+    <div className='card'>
+        <div className='poster'>
+            <img src={movie.poster_path ? API_IMG + movie.poster_path : "https://images.unsplash.com/photo-1604975701397-6365ccbd028a?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt='movie' />
+        </div>
+        <div className='info'>
+            <p className='title'>
+                {movie.title}
+            </p>
+            <p className='vote'>
+                {movie.vote_average}
+            </p>
+        </div>
+
+        <div className='overview'>
+            <h2 className='title_overview'>
+                Overview
+            </h2>
+            <h3 className='overview_info'>{movie.overview}</h3>
+        </div>
+    </div>
+  )
+}
+
+export default MovieCard
